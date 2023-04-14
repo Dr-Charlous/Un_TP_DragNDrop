747f4f6229e2700cca1ea354ac5178dda283e346
Dr-Charlous <charles.piercourt@outlook.fr>
2023-04-14 10:28:23 +0200

    Drag UI

--- Assets/Script/DragUI.cs
+++ Assets/Script/DragUI.cs
@@ -5,40 +5,52 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Image))]
-public class DragUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IDragHandler, IEndDragHandler
+[RequireComponent(typeof(RectTransform))]
+public class DragUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    [SerializeField] Image image;
+    Image image;
     [SerializeField] Color colorOnHover;
     Color initialColor;
-    Vector3 initialPosition;
+    //Vector3 initialPosition;
+    RectTransform rectTransform;
+    Vector2 initialPosition;
+    Canvas canvas;
 
     void Start()
     {
-        initialPosition = transform.position;
+        //initialPosition = transform.position;
         image = gameObject.GetComponent<Image>();
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
         if (image == null) return;
         initialColor = image.color;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        print("Pointer Enter");
+        //print("Pointer Enter");
         image.color = colorOnHover;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        print("Pointer Exit");
+        //print("Pointer Exit");
         image.color = initialColor;
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        initialPosition = rectTransform.anchoredPosition;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        gameObject.transform.position = Input.mousePosition;
+        //gameObject.transform.position = Input.mousePosition;
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        gameObject.transform.position = initialPosition;
+        //gameObject.transform.position = initialPosition;
+        rectTransform.anchoredPosition = initialPosition;
     }
 }
